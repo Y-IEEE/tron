bf43918603a673769870a9134528dbd6aabf8752
ErikBoesen <me@erikboesen.com>
2019-09-07 20:10:22 -0400

    Clean up app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,24 +4,6 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
-mail = Mail(app)
-
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('App startup')
 
 from app import routes, models, errors, api
 app.register_blueprint(api.api_blueprint, url_prefix='/api')
