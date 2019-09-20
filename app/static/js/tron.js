da4bde48d7af0ba797d7333cce47276d91655e34
ErikBoesen <me@erikboesen.com>
2019-09-20 15:05:30 -0400

    Get board from redis and write

--- app/static/js/tron.js
+++ app/static/js/tron.js
@@ -1,5 +1,5 @@
 const WIDTH = 64,
-      HEIGHT = 32,
+      HEIGHT = 36,
       PIXEL_SIZE = 10;
 
 const canvas = document.getElementById('tron');
@@ -12,10 +12,11 @@ fetch('/api/board')
     return response.json();
 })
 .then((colors) => {
-    for (let row = 0; row < colors.length; row++) {
-        for (let col = 0; col < colors[row].length; col++) {
-            ctx.fillStyle = '#' + colors[row][col];
-            ctx.fillRect(PIXEL_SIZE * row, PIXEL_SIZE * col, PIXEL_SIZE, PIXEL_SIZE);
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
+            ctx.fillStyle = '#' + colors[WIDTH * row + col];
+            ctx.fillRect(PIXEL_SIZE * col, PIXEL_SIZE * row, PIXEL_SIZE, PIXEL_SIZE);
         }
     }
+    console.log(colors);
 });
